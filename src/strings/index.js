7834591546819bba10dac360c42abebdfde5293c
Valerio Evangelisti <valerio.evangelisti@mia-platform.eu>
2021-04-19 11:46:53 +0200

    minor fixes on gitignore, package.json and others

--- src/strings/index.js
+++ src/strings/index.js
@@ -3,6 +3,7 @@
 *  If your app is not multi-language, use only a language and you can import it directly
 */
 
+// eslint-disable-next-line
 export default {
   en: () => import('./locales/en.json'),
   it: () => import('./locales/it.json')
