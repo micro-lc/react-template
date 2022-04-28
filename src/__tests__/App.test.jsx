a3a0a55cb5e153ed6d752db29ad7502113f9d614
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-04-28 10:40:18 +0200

    Lint

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -15,7 +15,7 @@
  */
 
 import React from 'react'
-import { screen } from '@testing-library/react'
+import {screen} from '@testing-library/react'
 
 import {renderWrapperComponent} from '../utilsTests'
 import App from '../App'
--- src/strings/index.js
+++ src/strings/index.js
@@ -3,7 +3,9 @@
 *  If your app is not multi-language, use only a language and you can import it directly
 */
 
-export default {
+const strings = {
   en: () => import('./locales/en.json'),
   it: () => import('./locales/it.json')
 }
+
+export default strings
